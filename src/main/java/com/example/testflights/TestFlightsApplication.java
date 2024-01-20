1bc40c20d4f4ea552a6ee0916db3510caebfc6f6
elizaveta <l1zail@yandex.ru>
2024-01-20 07:26:33 +0300

    first commit

---
+++ src/main/java/com/example/testflights/TestFlightsApplication.java
@@ -0,0 +1,13 @@
+package com.example.testflights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestFlightsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestFlightsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testflights/TestFlightsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testflights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestFlightsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
